92c5681343d2b974db0b5d4649b6b6607665ea18
Eduardo Trujillo <129311642+Lalo64GG@users.noreply.github.com>
2024-07-25 00:58:59 -0600

    chore: Update server to use HTTPS with SSL certificate

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from "express";
 import { getSensorData } from "./app"; // Asegúrate de que la ruta sea correcta
 import cors from "cors";
 import dotenv from "dotenv";
+import https from "https";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
 
@@ -9,18 +12,25 @@ const main = async () => {
   try {
     const app = express();
     app.use(cors());
-    const port = 5000;
+
+    const PORT = process.env.PORT || 5000; // Puedes configurar el puerto en el archivo .env si lo deseas
+    
+    const httpsOptions = {
+      key: fs.readFileSync(path.join(__dirname, '../../../../etc/nginx/ssl/nginx-selfsigned.key')),
+      cert: fs.readFileSync(path.join(__dirname, '../../../../etc/nginx/ssl/nginx-selfsigned.crt'))
+    };
 
     app.get("/api/gas-level", (req: Request, res: Response) => {
       res.json({ 
         gas_level: getSensorData().gas_level,
         temperature: getSensorData().temperature
-       });
+      });
     });
 
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+    https.createServer(httpsOptions, app).listen(PORT, () => {
+      console.log(`Server is running on https://localhost:${PORT}`);
     });
+
   } catch (error) {
     if (error instanceof Error) console.log(error.message);
   }
